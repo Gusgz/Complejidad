5f74af4723c8e91e94d10eadff6c6207b990776e
gustavo <gustavozegarra998@hotmail.com>
2020-06-22 19:12:38 -0500

    nuevo rep

--- Computer.py
+++
@@ -1,8 +0,0 @@
-import random
-class Computer:
-    def __init__(self):
-        self.position = random.randint(0,4)
-        self.name = ""
-        self.points = 0
-    def move(self):
-        self.position = random.randint(0,4)
--- Gemas.py
+++
@@ -1,6 +0,0 @@
-class Gemas:
-    def __init__(self):
-        self.key = 0
-        self.color = ""
-        self.points = 0
-        
